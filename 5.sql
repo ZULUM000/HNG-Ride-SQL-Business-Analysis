f1c0174b7b07eac094a5cd5b34c9b53ebed5cab9
Enoch Chukwuebuka Jeremiah <93448925+ZULUM000@users.noreply.github.com>
2025-10-24 22:30:39 +0100

    Delete 5.sql

--- 5.sql
+++
@@ -1,6 +0,0 @@
-SELECT pickup_city,
-       ROUND(100.0 * SUM(CASE WHEN status = 'cancelled' THEN 1 ELSE 0 END) / COUNT(*),2) AS cancellation_rate_percent
-FROM rides
-GROUP BY pickup_city
-ORDER BY cancellation_rate_percent DESC
-LIMIT 10;
