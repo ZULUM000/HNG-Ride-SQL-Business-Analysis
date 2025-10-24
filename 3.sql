012d6506438a15ecbd45faeff8b49293d69cb593
Enoch Chukwuebuka Jeremiah <93448925+ZULUM000@users.noreply.github.com>
2025-10-24 22:30:05 +0100

    Delete 3.sql

--- 3.sql
+++
@@ -1,6 +0,0 @@
-SELECT EXTRACT(YEAR FROM r.request_time)::INT AS year,
-       EXTRACT(QUARTER FROM r.request_time)::INT AS quarter,
-       SUM(r.fare) AS total_revenue
-FROM rides r
-GROUP BY year, quarter
-ORDER BY year, quarter;
