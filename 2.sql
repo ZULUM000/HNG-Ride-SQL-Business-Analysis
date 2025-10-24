609b2673501dd90eba2c5b07f790c9d8efcbe61f
Enoch Chukwuebuka Jeremiah <93448925+ZULUM000@users.noreply.github.com>
2025-10-24 22:29:45 +0100

    Delete 2.sql

--- 2.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(DISTINCT r.rider_id) AS active_riders_2024
-FROM riders r
-JOIN rides ri ON r.rider_id = ri.rider_id
-WHERE EXTRACT(YEAR FROM r.signup_date) = 2021
-  AND EXTRACT(YEAR FROM ri.request_time) = 2024;
